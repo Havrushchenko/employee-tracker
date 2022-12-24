1df72795c3ad5911c7eded3a14ef37ae3ecbaf3f
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-12-24 17:42:42 +0200

    added ability to show roles and departments in tables

--- server.js
+++ server.js
@@ -1,59 +1,82 @@
+const mysql = require('mysql2');
 const inquirer = require("inquirer");
 const cTable = require('console.table');
-const mysql = require('mysql2');
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
         host: 'localhost',
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: '',
+        password: 'bonik566569',
         database: 'employee'
     },
-    console.log('Connected to the election database.')
+    console.log('Connected to the employee database.')
 );
 
-const questions = [
-    {
-        type: 'list',
-        name: 'choise',
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments.',
-            'View all roles.',
-            'View all employees.',
-            'Add a department.',
-            'Add a role.',
-            'Add an employee.',
-            'Update an employee role.',
-        ],
-        validate: choiceList => {
-            if (choiceList) {
-                return true;
-            } else {
-                console.log('You must select one from the list!');
-                return false;
+const questions = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments.',
+                'View all roles.',
+                'View all employees.',
+                'Add a department.',
+                'Add a role.',
+                'Add an employee.',
+                'Update an employee role.'
+            ],
+            validate: choicesList => {
+                if (choicesList) {
+                    return true;
+                } else {
+                    console.log('You must select one from the list!');
+                    return false;
+                }
             }
         }
-    }
-];
+    ]).then(function (answers) {
+        const { choices } = answers;
+        if (choices === "View all departments.") {
+            showDepartments();
+        }
+        if (choices === "View all roles.") {
+            showRoles();
+        }
+        if (choices === "View all employees.") {
+            showEmployees();
+        }
+    });
+};
 
-inquirer.prompt(questions).then((answers) => {
-    console.log('\nOrder receipt:');
-    console.log(JSON.stringify(answers, null, '  '));
-});
+function showDepartments() {
+    console.log('Showing all departments');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        questions();
+    })
+};
 
-app.use((req, res) => {
-    res.status(404).end();
-});
+function showRoles() {
+    console.log('Showing all roles');
+    const sql = `SELECT role.id, role.title, role.salary, department.name AS department
+    FROM role
+    JOIN department ON role.department_id = department.id`;
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        questions();
+    })
+};
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+
+
+
+

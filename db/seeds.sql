a43dab33d3032700fe3b1852bbe3022197e88440
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-12-25 14:48:53 +0200

    updated seeds.sql values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,28 @@
 employeeeINSERT INTO department (name)
 VALUES
-('Dept-1'),
-('Dept-2'),
-('Dept-3'),
-('Dept-4'),
-('Dept-5');
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Role-1', 1000, 1),
-('Role-2', 2000, 5),
-('Role-3', 3000, 3),
-('Role-4', 4000, 4),
-('Role-5', 5000, 2);
+('Full Stack Developer', 100000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Finanical Analyst', 150000, 2),
+('Marketing Coordindator', 70000, 3), 
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ronald', 'Firbank', 1, 3),
-  ('Virginia', 'Woolf', 1, 2),
-  ('Piers', 'Gaveston', 0, 1),
-  ('Charles', 'LeRoi', 1, 5),
-  ('Katherine', 'Mansfield', 1, 4);
+('Mark', 'Miller', 2, null),
+('Devin', 'Anderson', 1, 1),
+('Mary', 'Brown', 4, null),
+('Ashley', 'Jones', 3, 3),
+('Tyler', 'Moore', 6, null),
+('Ana', 'Sanchez', 5, 5),
+('Lewis', 'Allen', 7, null),
+('Katherine', 'Green', 8, 7);
